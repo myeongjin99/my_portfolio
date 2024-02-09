4d5aeca5bd1ee7a936a72e902250a66c2cccef6c
seo myeong jin <seomyeongjin99@gmail.com>
2024-02-09 23:14:05 +0900

    <Feat : 헤더 반응형 구현 완료>

---
+++ src/components/Header.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import menu from "../assets/images/category.png";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleReSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleReSize);
+    return () => window.removeEventListener("resize", handleReSize);
+  }, []);
+
+  const menuOnclick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <HeaderContainer>
+      <Logo>JINI'S WEBFOLIO</Logo>
+      {isMobile ? (
+        <div>
+          <Menu src={menu} alt="" onClick={menuOnclick} />
+        </div>
+      ) : (
+        <CategoryContainer>
+          <Category>ABOUT ME</Category>
+          <Category>RESULT</Category>
+          <Category>STACKS</Category>
+          <Category>SNS</Category>
+        </CategoryContainer>
+      )}
+      {isMobile && isMenuOpen && (
+        <CategoryContainer>
+          <Category>ABOUT ME</Category>
+          <Category>RESULT</Category>
+          <Category>STACKS</Category>
+          <Category>SNS</Category>
+        </CategoryContainer>
+      )}
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled.div`
+  display: flex;
+  height: 70px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  padding: 0px 40px 0px 40px;
+  @media screen and (max-width: 600px) {
+    display: flex;
+    /* flex-direction: column; */
+  }
+`;
+
+const Logo = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  font-family: "ChosunGs";
+  font-size: 23px;
+  font-weight: 500;
+`;
+
+const Menu = styled.img`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+`;
+const CategoryContainer = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  @media screen and (max-width: 768px) {
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    width: 100%;
+    top: 70px;
+    left: 0px;
+    padding: 10px 0px 10px 0px;
+  }
+`;
+
+const Category = styled.div`
+  /* border: 1px solid blue; */
+  font-family: "ChosunGs";
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export default Header;
